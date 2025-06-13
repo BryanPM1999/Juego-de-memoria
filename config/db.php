1855616cf8b14fcca16cab2a4bf2a9b1db976008
alexander <jr785497@gmail.com>
2025-06-13 11:20:08 -0600

    se elimino el .env y se agrego el retorno de la conexion de la base de datos

---
+++ config/db.php
@@ -0,0 +1,26 @@
+<?php
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
+function conectar() {
+
+    $host = $_ENV['DB_HOST'];
+    $port = $_ENV['DB_PORT'];
+    $db   = $_ENV['DB_DATABASE'];
+    $user = $_ENV['DB_USERNAME'];
+    $pass = $_ENV['DB_PASSWORD'];
+
+    $conn = new mysqli($host, $user, $pass, $db, $port);
+
+    if ($conn->connect_error) {
+        
+        die("Conexión fallida: " . $conn->connect_error);
+    }
+    $conn->set_charset("utf8");
+
+    return $conn;
+}
+?>
+
--- public/config/test_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'db.php';
-
-if ($conn->ping()) {
-    echo "Conexión exitosa a la base de datos";
-} else {
-    echo "Error en la conexión: " . $conn->error;
-}
